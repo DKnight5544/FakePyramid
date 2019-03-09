b89d543f773773b98c156094f6f1c2847bffa5fa
DKnight5544 <dave@mostlyspam.com>
2019-03-08 21:00:01 -0800

    Add project files.

---
+++ FakePyramid/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FakePyramid.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index(string id)
+        {
+
+            if (string.IsNullOrWhiteSpace(id)) id = "WIGIWIZ";
+
+            using (DWKDBDataContext db = new DWKDBDataContext())
+            {
+
+                var idParts = id.Split('|');
+                string userID = idParts[0];
+
+                //if there is a second parameter then we need to insert the
+                //new user before we display the page.
+                if (idParts.Length > 1)
+                {
+                    string newUserID = idParts[1];
+                    db.User_Insert(userID, newUserID);
+                    Response.Redirect("/" + newUserID);
+                }
+
+                //grab the user data and pass it to the view
+                var userData = db.User_SelectByUserID(userID).SingleOrDefault();
+
+                return View(userData);
+
+            }
+        }
+    }
+}
---
+++ GenerateTestData.sql
@@ -0,0 +1,90 @@
+﻿
+
+delete from [FP].[User]
+insert into FP.[User] (UserID, ParentID,PayeeID,L1,JoinDateTime) select 'wigiwiz', 'wigiwiz', 'wigiwiz',2, '2019-03-03 00:00:01.000'
+insert into FP.[User] (UserID, ParentID,PayeeID,L1,JoinDateTime) select 'dougraley', 'wigiwiz', 'wigiwiz',0, '2019-03-03 00:00:02.000'
+insert into FP.[User] (UserID, ParentID,PayeeID,L1,JoinDateTime) select 'joleneknight', 'wigiwiz', 'wigiwiz',0, '2019-03-03 00:00:03.000'
+
+declare @return int;
+exec @return = [FP].[User_Insert] 'wigiwiz', '0101'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0101', '0201'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0201','0301'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0301','0401'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0401','0501'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0501','0601'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0601','0701'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0201','0702'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0501','0602'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0602','0703'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] 'joleneknight','0202'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0202','0302'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0302','0402'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0402','0502'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0502','0603'
+select [Return Value] = @return;
+
+--declare @return int;
+exec @return = [FP].[User_Insert] '0502','0604'
+select [Return Value] = @return;
+
+declare @return int;
+exec @return = [FP].[User_Insert] 'dougraley','0203'
+select [Return Value] = @return;
+
+declare @return int;
+exec @return = [FP].[User_Insert] '0203','0303'
+select [Return Value] = @return;
+
+declare @return int;
+exec @return = [FP].[User_Insert] '0303','0403'
+select [Return Value] = @return;
+
+declare @return int;
+exec @return = [FP].[User_Insert] '0403','0503'
+select [Return Value] = @return;
+
+declare @return int;
+exec @return = [FP].[User_Insert] '0503','0605'
+select [Return Value] = @return;
